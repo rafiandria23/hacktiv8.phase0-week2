ecbc62cd23200d0cad1d152cfc3e97e3f5f3bf29
Adam Rafiandri <rafiandria23@gmail.com>
2019-11-20 15:50:40 +0700

    solved all the problems

---
+++ balik-kata.js
@@ -0,0 +1,22 @@
+/**
+ * HACKTIV8 Phase 0 - Menggunakan Switch Case
+ * Code Skeleton made by HACKTIV8 Team, solved by Adam Rafiandri
+ */
+
+
+
+
+
+ function balikKata(kata) {
+    var output = '';
+    for (let index = kata.length - 1; index >= 0; index--) {
+        output += kata[index];
+    }
+    return output;
+ }
+
+console.log(balikKata('Hello World and Coders'));
+console.log(balikKata('John Doe'));
+console.log(balikKata('I am a bookworm'));
+console.log(balikKata('Coding is my hobby'));
+console.log(balikKata('Super'));
